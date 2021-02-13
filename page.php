55f766c97c42f28340e1f4bd5181d06853227427
juan gallardo <juan@texdinamo.com>
2021-02-13 16:55:21 -0300

    agregando linsk de subpaginas

--- page.php
+++ page.php
@@ -16,15 +16,43 @@
 
   <div class="container container--narrow page-section">
 
-    <!--
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink(wp_get_post_parent_id(get_the_ID())); ?>"> <?php echo get_the_title(wp_get_post_parent_id(get_the_ID())); ?> </a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+      <p><?php
+      
+$theParent = wp_get_post_parent_id(get_the_ID()); 
+
+if($theParent){
+  $findChildrenOf = $theParent;
+  echo $findChildrenOf . ' existe';
+}else{
+  $findChildrenOf = get_the_ID();
+  echo get_the_ID() . ' no existe ';
+}
+
+      
+      ?></p>
+      
+      <?php
+
+$theParent = wp_get_post_parent_id(get_the_ID()); 
+
+
+        if($theParent){
+          $findChildrenOf = $theParent;
+        }else{
+          $findChildrenOf = get_the_ID();
+        }
+
+        wp_list_pages(array(
+          'title_li' => null,
+          'child_of' => $findChildrenOf
+        ))
+
+      ?>
       </ul>
     </div>
--->
 
 <?php
 
@@ -43,6 +71,9 @@ $theParent = wp_get_post_parent_id(get_the_ID());
 </div>
 
   </div>
+
+
+  
     <?php }
 
     get_footer();
