0cac998f233762be76471dbab70b77676026e90c
juan gallardo <juan@texdinamo.com>
2021-02-15 20:28:50 -0300

    guardando archive

--- archive.php
+++ archive.php
@@ -7,19 +7,18 @@ get_header();
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php
+      <h1 class="page-banner__title">
       
-      if(is_category()){
-        single_cat_title();
-      }
+        <?php
 
-      if(is_author()){
-          echo 'Posts by '; the_author();
-      }
+            the_archive_title();
+        
+        
+        ?>
       
-      ?></h1>
+      </h1>
       <div class="page-banner__intro">
-        <p>Learn how the school of your dreams got started.</p>
+        <p><?php the_archive_description(); ?></p>
       </div>
     </div>  
   </div>
