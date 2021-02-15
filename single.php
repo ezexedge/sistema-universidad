55b36b97a217e72d9724be2028827846998ea0d3
juan gallardo <juan@texdinamo.com>
2021-02-15 20:09:29 -0300

    guardando blog

--- single.php
+++ single.php
@@ -4,11 +4,44 @@
 
     while(have_posts()){
      the_post(); ?>
-     <h2><?php the_title(); ?></h2>   
-        <?php the_content(); ?>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p>Learn how the school of your dreams got started.</p>
+      </div>
+    </div>  
+  </div>
 
 
-    <?php }
 
-    get_footer();
+  <div class="container container--narrow page-section">
+
+
+<?php
+
+$theParent = wp_get_post_parent_id(get_the_ID()); 
+
+if(!$theParent) { ?>
+
+<div class="metabox metabox--position-up metabox--with-home-link">
+  <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to blog </a> <span class="metabox__main"><?php the_title();?></span></p>
+</div>
+<?php 
+}
+?>
+<div class="generic-content">
+    <?php the_content(); ?>
+</div>
+
+</div>
+
+
+
+<?php }
+
+get_footer();
+
 ?>
