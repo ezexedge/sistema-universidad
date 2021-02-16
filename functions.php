fcb51c847bee3341c2e4abe4939f1e7ad4b4570c
juan gallardo <juan@texdinamo.com>
2021-02-16 11:59:47 -0300

    agregando postypes

--- functions.php
+++ functions.php
@@ -2,10 +2,10 @@
 
 
 function university_files(){
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'),NULL,'1.0',true);
     wp_enqueue_style('custom-google-font','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js' ,NULL,'1.0',true);
+//    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 
 }
 
@@ -18,4 +18,7 @@ function university_features(){
 }
 
 add_action('after_setup_theme','university_features');
+
+
+
 ?>
